2efd2a012aa3e68897ed2246a6ab7447cb8c6eb2
gyu98 <gyu163425@gmail.com>
2018-03-04 17:11:00 +0800

    图片上传与下载

--- src/main/java/com/flower/controller/AllFlowerController.java
+++ src/main/java/com/flower/controller/AllFlowerController.java
@@ -15,8 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.Date;
 import java.util.List;
 
@@ -47,7 +47,7 @@ public class AllFlowerController extends  BaseController {
             String []lastName=request.getFile().getOriginalFilename().split("\\.");
             Date date=new Date();
             String fileName=String.valueOf(date.getTime()/1000)+"."+lastName[1];
-            String filePath = "E:\\imgDownload\\";
+            String filePath = "D:\\imgDownload\\";
             try {
                 FileCopyUtils.copy(request.getFile().getInputStream(),new FileOutputStream(filePath+fileName));
                 request.setPicUrl(filePath+fileName);
@@ -58,6 +58,44 @@ public class AllFlowerController extends  BaseController {
 
         return allFlowerService.addFlower(request);
     }
+
+    /**
+     * 文件下载
+     * @param res
+     */
+    @RequestMapping(value = "/downloadImg", method = RequestMethod.GET)
+    public void testDownload(HttpServletResponse res) {
+        String fileName = "upload.jpg";
+        res.setHeader("content-type", "application/octet-stream");
+        res.setContentType("application/octet-stream");
+        res.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+        byte[] buff = new byte[1024];
+        BufferedInputStream bis = null;
+        OutputStream os = null;
+        try {
+            os = res.getOutputStream();
+            bis = new BufferedInputStream(new FileInputStream(new File("d://"
+                    + fileName)));
+            int i = bis.read(buff);
+            while (i != -1) {
+                os.write(buff, 0, buff.length);
+                os.flush();
+                i = bis.read(buff);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("success");
+    }
+
 
 
 
